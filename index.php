fdc495d87f0606f616313983e4e15172915f300f
gomeow <davismariotti@gmail.com>
2013-02-03 11:37:37 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>GOMEOW</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<h1>My first website!</h1>
+		<p>Brought to you by gomeow!</p>
+	</body>
+</html>
