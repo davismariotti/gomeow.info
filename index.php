0114eb097370bae5263b03bcd483355fe8d7fbc5
gomeow <davismariotti@gmail.com>
2013-02-03 21:03:31 -0800

    Test update script

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
 		<div class="container">
 			<div class="well" style="height: 300px;">
 				<h1>GOMEOW!</h1>
-				I am gomeow. Inspector gomeow.
+				<p>I am gomeow. Inspector gomeow.</p>
+				<p>(Put a better description here)</p>
 				<img class="thumbnail img-gomeow" src = "http://forums.bukkit.org/data/avatars/l/90728/90728305.jpg?1359043699" width = "180" height = "180" />
 			</div>
 		</div>
