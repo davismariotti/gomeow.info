593f66c68a22ef69b9b75bd9f588cc64e88602e6
gomeow <davismariotti@gmail.com>
2013-02-04 20:16:18 -0800

    ADD LOTS OF THINGS!

--- index.php
+++ index.php
@@ -1,5 +1,74 @@
 <?php
-	require("functions.php"); 
+	require("functions.php");
+	require("../private/config.php");
+	
+	function connectDB($user, $pass, $db) {
+	try {	
+		return(new PDO("mysql:host=localhost;dbname=" . $db . ";charset=utf8", $user, $pass));
+	} catch(PDOException $ex) {
+		return $ex;
+	}
+}
+$visits = 1;
+$db = connectDB($dbUser, $dbPass, $dbName);
+if ($db instanceof PDOException) {
+	die ($db->getMessage());
+}
+$ip = $_SERVER['REMOTE_ADDR'];
+$sql = "SELECT * FROM `IPS` WHERE `IP` = :ip LIMIT 1"; 
+$stmt = $db->prepare($sql);
+$stmt->bindParam(':ip', $ip);
+$stmt->execute();
+if($stmt->rowCount() == 0) {
+	$sql = "INSERT INTO `IPS`(`IP`, `Visits`) VALUES (:ip, 1)"; 
+	$stmt = $db->prepare($sql);
+	$stmt->bindParam(':ip', $ip);
+	$stmt->execute();
+	
+	$sql = "SELECT * FROM `Stats` WHERE 1"; 
+	$stmt = $db->prepare($sql);
+	$stmt->execute();
+	$row = $stmt->fetch();
+	$TotalHits = $row['TotalHits'];
+	$TotalHits = $TotalHits + 1;
+	$TotalUniqueVisits = $row['TotalUniqueVisits'];
+	$TotalUniqueVisits = $TotalUniqueVisits + 1;
+	
+	$sql = "UPDATE `Stats` SET `TotalHits` = :value1 , `TotalUniqueVisits` =  :value2 WHERE 1"; 
+	$stmt = $db->prepare($sql);
+	$stmt->bindParam(':value1', $TotalHits);
+	$stmt->bindParam(':value2', $TotalUniqueVisits);
+	$stmt->execute();
+}
+else {
+	$sql = "SELECT * FROM `IPS` WHERE `IP` = :ip LIMIT 1"; 
+	$stmt = $db->prepare($sql);
+	$stmt->bindParam(':ip', $ip);
+	$stmt->execute();
+	$row = $stmt->fetch();
+	$visits = $row['Visits'];
+	$visits = $visits + 1;
+	
+	$sql = "UPDATE `IPS` SET `Visits` = :visits WHERE `IP` = :ip"; 
+	$stmt = $db->prepare($sql);
+	$stmt->bindParam(':ip', $ip);
+	$stmt->bindParam(':visits', $visits);
+	$stmt->execute();
+	
+	$sql = "SELECT * FROM `Stats` WHERE 1"; 
+	$stmt = $db->prepare($sql);
+	$stmt->execute();
+	$row = $stmt->fetch();
+	$TotalHits = $row['TotalHits'];
+	$TotalHits = $TotalHits + 1;
+	$TotalUniqueVisits = $row['TotalUniqueVisits'];
+	
+	$sql = "UPDATE `Stats` SET `TotalHits` = :value WHERE 1"; 
+	$stmt = $db->prepare($sql);
+	$stmt->bindParam(':value', $TotalHits);
+	$stmt->execute();
+	
+}
 ?>
 <html>
 	<head>
@@ -12,7 +81,8 @@
 			<div class="well" style="height: 300px;">
 				<h1>GOMEOW!</h1>
 				<p>I am gomeow. Inspector gomeow.</p>
-				<p>(Put a better description here)</p>
+				<p>You have visited me  <?php echo $visits ?> times!</p>
+				<p>This site has had <?php echo $TotalHits; ?> total hits with <?php echo $TotalUniqueVisits; ?> unique visits!</p>
 				<img class="thumbnail img-gomeow" src = "http://forums.bukkit.org/data/avatars/l/90728/90728305.jpg?1359043699" width = "180" height = "180" />
 			</div>
 		</div>
