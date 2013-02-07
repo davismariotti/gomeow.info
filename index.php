a34fb070046d60643af19c87045c82f74b04e49c
lol768 <support@technostripe.com>
2013-02-07 19:13:53 +0000

    Switch to clas based content block

--- index.php
+++ index.php
@@ -92,7 +92,7 @@ else {
 	<body>
 		<?php navBar(); ?>
 		<div class="container">
-			<div class="well" style="min-height: 200px;">
+			<div class="well mainContent">
 			<img class="thumbnail img-gomeow" src = "http://forums.bukkit.org/data/avatars/l/90728/90728305.jpg?1359043699" width = "180" height = "180" />
 
 				<h1>GOMEOW!</h1>
