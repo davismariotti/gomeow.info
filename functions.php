75d8cf3712268695e1ee12369dbf3291b5ca8559
gomeow <davismariotti@gmail.com>
2013-02-09 17:23:59 -0800

    Change an bar to only highlight current page

--- functions.php
+++ functions.php
@@ -9,15 +9,15 @@ function headIncludes() {
 }
 
 function navBar() {
+	$parent = basename($_SERVER['PHP_SELF']);
 	?>
 	<div class="navbar">
 		<div class="navbar-inner">
 			<a class="brand" href="index.php">Gomeow</a>
 			<ul class="nav">
-				<li class="active"><a href="index.php">Home</a></li>
+				<li <?php if($parent == "index.php") { ?>class="active" <?php } ?>><a href="index.php">Home</a></li>
 				<li class="divider-vertical"></li>
-				<li><a href="plugins.php">My Plugins</a></li>
-				<li class="divider-vertical"></li>
+				<li <?php if($parent == "plugins.php") { ?>class="active" <?php } ?>><a href="plugins.php">My Plugins</a></li>
 				<li><a href="#">Put other Link here</a></li>
 			</ul>
 		</div>
