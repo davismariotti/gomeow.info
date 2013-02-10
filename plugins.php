0c1bbfa3312bd9486ba16c3e1e7c6579c3c1a672
gomeow <davismariotti@gmail.com>
2013-02-09 17:30:39 -0800

    Create plugins.php

---
+++ plugins.php
@@ -0,0 +1,21 @@
+<?php
+	include("functions.php");
+?>
+<html>
+	<head>
+		<title>My Plugins</title>
+		<?php headIncludes(); ?>
+	</head>
+	<body>
+		<?php navBar(); ?>
+		<div class="container">
+			<div class="well mainContent">
+				<h2>My Plugins</h2>
+				<ul>
+					<li>Deposit Chests</li>
+					<li>Add more here</li>
+				</ul>
+			</div>
+		</div>
+	</body>
+</html>
