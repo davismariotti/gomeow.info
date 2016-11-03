ee8db823f6aad67ceaf01c29911823c97e4f6ef7
Davis Mariotti <davismariotti@gmail.com>
2016-11-03 12:30:14 -0700

    Relocate avatar to local server

--- index.php
+++ index.php
@@ -93,7 +93,7 @@
 		<?php navBar(); ?>
 		<div class="container">
 			<div class="well mainContent">
-			<img class="thumbnail img-gomeow" src = "http://forums.bukkit.org/data/avatars/l/90728/90728305.jpg?1359043699" width = "180" height = "180" />
+			<img class="thumbnail img-gomeow" src = "files/90728305.jpg" width = "180" height = "180" />
 
 				<h1>GOMEOW!</h1>
 				<p>I am gomeow. Inspector gomeow.</p>
